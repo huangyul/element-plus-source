0930bc6d58c89d99191cd26cd1ac231d7bc94b67
huangyul <1103221645@qq.com>
2022-12-08 14:54:38 +0800

    feat: add dayjs

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,6 @@ import Vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [Vue()],
   test: {
-    environment: 'happy-dom',
+    environment: 'happy-dom', // 测试环境，如果是web端的测试，可以使用happdom或jsdom
   },
 })

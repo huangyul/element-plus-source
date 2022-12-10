a345405c6f0ab8ee0b3417dc32d5fc43f2b0a6e6
huangyul <1103221645@qq.com>
2022-12-10 09:39:07 +0800

    feat: add jest basic config

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+  preset: "ts-jest",
+  globals: {
+    // work around: https://github.com/kulshekhar/ts-jest/issues/748#issuecomment-423528659
+    "ts-jest": {
+      diagnostics: {
+        ignoreCodes: [151001],
+      },
+    },
+  },
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.vue$": "vue-jest",
+  },
+  moduleFileExtensions: ["vue", "json", "ts", "tsx", "js", "json"],
+};

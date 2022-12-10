2e92d67d90ff611b2a01c0b1768da1761821747b
huangyul <1103221645@qq.com>
2022-12-10 09:55:44 +0800

    feat: add the first test case

--- packages/button/__tests__/button.spec.ts
+++ packages/button/__tests__/button.spec.ts
@@ -1,6 +1,38 @@
+import { mount } from "@vue/test-utils";
+import Button from "../src/index.vue";
+
+const AXIOM = "Rem is the best girl";
+const COMMON_CONFIG = {
+  global: {
+    provide: {
+      elForm: {},
+      elFormItem: {},
+    },
+  },
+};
+
+// 定义
 describe("Button.vue", () => {
-  test("hello world", () => {
-    const bestGirl = "Rem";
-    expect(bestGirl).not.toMatch("Emilia");
+  // 测试一，渲染文本
+  test("render text", () => {
+    const instance = mount(Button, {
+      slots: {
+        default: AXIOM,
+      },
+      ...COMMON_CONFIG,
+    });
+    expect(instance.text()).toEqual(AXIOM);
+  });
+
+  // 测试二 模拟点击
+  test("handle click", async () => {
+    const instance = mount(Button, {
+      slots: {
+        default: AXIOM,
+      },
+      ...COMMON_CONFIG,
+    });
+    await instance.trigger("click");
+    expect(instance.emitted()).toBeDefined();
   });
 });

e51330649a49a3ade89caa3ff69835e31898fa4a
huangyul <1103221645@qq.com>
2022-12-12 10:51:15 +0800

    feat: add story source

--- .storybook/export-loader.js
+++ .storybook/export-loader.js
@@ -1,9 +1,9 @@
-module.exports = function (content) {
-  return content.replace(
+module.exports = function(content, map, meta) {
+  constent = content.replace(
     /export ?{\s* default as (.+?)}(.+)/g,
-    `
-import _$1 $2
-export const $1 = () => _$1
-`
+    `import _$1 $2
+     export const $1 = () => _$1`.trim()
   );
+  this.callback(null, content, map, meta);
+  return;
 };
--- packages/badge/doc/index.stories.ts
+++ packages/badge/doc/index.stories.ts
@@ -1,6 +1,11 @@
-
-export { default as BasicUsage} from './basic.vue'
+export { default as BasicUsage } from './basic.vue';
 
 export default {
-  title: "Badge",
-}
+  title: 'Badge'
+};
+
+export const AAA = () => `
+<el-badge :value="333">
+  <el-button>eee</el-button>
+</el-badge>
+`;

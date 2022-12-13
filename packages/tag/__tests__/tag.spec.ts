73d8b7d7eebcb77e33aa48088dadb23b3864776c
huangyul <1103221645@qq.com>
2022-12-13 10:30:55 +0800

    feat: perfect tag test

--- packages/tag/__tests__/tag.spec.ts
+++ packages/tag/__tests__/tag.spec.ts
@@ -1,16 +1,16 @@
-import {mount} from '@vue/test-utils'
-import Tag from '../src/index.vue'
+import { mount } from '@vue/test-utils';
+import Tag from '../src/index.vue';
 
-const AXIOM = 'Rem is the best girl'
+const AXIOM = 'Rem is the best girl';
 
 describe('Tag.vue', () => {
   test('render text & class', () => {
     const instance = mount(Tag, {
       slots: {
         default: AXIOM
-      },
-    })
-    expect(instance.text()).toEqual(AXIOM)
+      }
+    });
+    expect(instance.text()).toEqual(AXIOM);
 
     const vm = instance.vm;
 
@@ -18,68 +18,70 @@ describe('Tag.vue', () => {
     expect(vm.$el.classList.contains('el-tag__close')).toEqual(false);
     expect(vm.$el.classList.contains('is-hit')).toEqual(false);
     expect(vm.$el.classList.contains('md-fade-center')).toEqual(false);
-  })
+  });
 
   test('type', () => {
     const instance = mount(Tag, {
       props: {
         type: 'primary'
-      },
-    })
+      }
+    });
     const vm = instance.vm;
     expect(vm.$el.classList.contains('el-tag--primary')).toEqual(true);
-  })
+  });
 
   test('hit', () => {
     const instance = mount(Tag, {
       props: {
         hit: true
-      },
-    })
+      }
+    });
     const vm = instance.vm;
     expect(vm.$el.classList.contains('is-hit')).toEqual(true);
-  })
+  });
 
-  test('closeable', () => {
+  test('closeable', async () => {
     const instance = mount(Tag, {
       props: {
-        closable: true,
-      },
-    })
+        closable: true
+      }
+    });
     const closeBtn = instance.find('.el-tag .el-tag__close');
     expect(closeBtn.exists()).toBe(true);
-  })
+    await instance.trigger('click');
+    expect(instance.emitted().close).toBeTruthy();
+  });
 
   test('closeTransition', () => {
     const instance = mount(Tag, {
       props: {
-        closeTransition: true,
-      },
-    })
+        closeTransition: true
+      }
+    });
     const vm = instance.vm;
     expect(vm.$el.classList.contains('md-fade-center')).toEqual(false);
-  })
+  });
 
   test('color', () => {
     const instance = mount(Tag, {
       props: {
-        color: 'rgb(0, 0, 0)',
-      },
-    })
+        color: 'rgb(0, 0, 0)'
+      }
+    });
     const vm = instance.vm;
     expect(vm.$el.style.backgroundColor).toEqual('rgb(0, 0, 0)');
-  })
+  });
 
   test('theme', () => {
     const instance = mount(Tag, {
       props: {
-        effect: 'dark',
-      },
-    })
+        effect: 'dark'
+      }
+    });
     const vm = instance.vm;
     const el = vm.$el;
     expect(el.className.indexOf('el-tag--dark') > -1).toEqual(true);
     expect(el.className.indexOf('el-tag--light') > -1).toEqual(false);
     expect(el.className.indexOf('el-tag--plain') > -1).toEqual(false);
-  })
-})
+  });
+});

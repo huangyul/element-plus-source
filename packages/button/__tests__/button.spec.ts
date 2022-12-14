4e016b50a3203981d98e5332efadb23f452eb07a
huangyul <1103221645@qq.com>
2022-12-14 10:45:12 +0800

    feat(button): add button tests

--- packages/button/__tests__/button.spec.ts
+++ packages/button/__tests__/button.spec.ts
@@ -1,38 +1,136 @@
-import { mount } from "@vue/test-utils";
-import Button from "../src/index.vue";
+import { mount } from '@vue/test-utils';
+import Button from '../src/index.vue';
 
-const AXIOM = "Rem is the best girl";
+const AXIOM = 'Rem is the best girl';
 const COMMON_CONFIG = {
   global: {
     provide: {
       elForm: {},
-      elFormItem: {},
-    },
-  },
+      elFormItem: {}
+    }
+  }
 };
 
-// 定义
-describe("Button.vue", () => {
-  // 测试一，渲染文本
-  test("render text", () => {
+describe('Button.vue', () => {
+  it('create', () => {
+    const instance = mount(Button, {
+      props: { type: 'primary' },
+      ...COMMON_CONFIG
+    });
+    const buttonElm = instance.element;
+    expect(buttonElm.classList.contains('el-button--primary')).toBeTruthy();
+  });
+
+  it('icon', () => {
+    const instance = mount(Button, {
+      props: { icon: 'el-icon-search' },
+      ...COMMON_CONFIG
+    });
+    const buttonElm = instance.element;
+    expect(buttonElm.querySelector('.el-icon-search')).not.toBeUndefined();
+  });
+  it('nativeType', () => {
+    const instance = mount(Button, {
+      props: { nativeType: 'submit' },
+      ...COMMON_CONFIG
+    });
+    const buttonElm = instance.element;
+    expect(buttonElm.getAttribute('type')).toBe('submit');
+  });
+  it('loading', () => {
+    const instance = mount(Button, {
+      props: { loading: true },
+      ...COMMON_CONFIG
+    });
+    const buttonElm = instance.element;
+    expect(buttonElm.querySelector('.el-icon-search')).not.toBeUndefined();
+    expect(buttonElm.classList.contains('is-loading')).toBeTruthy();
+    expect(buttonElm.querySelector('.el-icon-loading')).not.toBeUndefined();
+  });
+  it('disabled', async () => {
+    const instance = mount(Button, {
+      props: { disabled: true },
+      ...COMMON_CONFIG
+    });
+    const buttonElm = instance.element;
+    expect(buttonElm.classList.contains('is-disabled')).toBeTruthy();
+    await instance.trigger('click');
+    expect(instance.emitted('click')).toBeUndefined();
+  });
+  it('size', () => {
+    const instance = mount(Button, {
+      props: { size: 'medium' },
+      ...COMMON_CONFIG
+    });
+    const buttonElm = instance.element;
+    expect(buttonElm.classList.contains('el-button--medium')).toBeTruthy();
+  });
+  it('plain', () => {
+    const instance = mount(Button, {
+      props: { plain: true },
+      ...COMMON_CONFIG
+    });
+    const buttonElm = instance.element;
+    expect(buttonElm.classList.contains('is-plain')).toBeTruthy();
+  });
+  it('round', () => {
+    const instance = mount(Button, {
+      props: { round: true },
+      ...COMMON_CONFIG
+    });
+    const buttonElm = instance.element;
+    expect(buttonElm.classList.contains('is-round')).toBeTruthy();
+  });
+  it('circle', () => {
+    const instance = mount(Button, {
+      props: { circle: true },
+      ...COMMON_CONFIG
+    });
+    const buttonElm = instance.element;
+    expect(buttonElm.classList.contains('is-circle')).toBeTruthy();
+  });
+
+  test('render text', () => {
     const instance = mount(Button, {
       slots: {
-        default: AXIOM,
+        default: AXIOM
       },
-      ...COMMON_CONFIG,
+      ...COMMON_CONFIG
     });
     expect(instance.text()).toEqual(AXIOM);
   });
 
-  // 测试二 模拟点击
-  test("handle click", async () => {
+  test('handle click', async () => {
     const instance = mount(Button, {
       slots: {
-        default: AXIOM,
+        default: AXIOM
       },
-      ...COMMON_CONFIG,
+      ...COMMON_CONFIG
     });
-    await instance.trigger("click");
+    await instance.trigger('click');
     expect(instance.emitted()).toBeDefined();
+  });
+
+  test('handle click inside', async () => {
+    const instance = mount(Button, {
+      slots: {
+        default: '<span class="inner-slot"></span>'
+      },
+      ...COMMON_CONFIG
+    });
+    await (<HTMLElement>instance.element.querySelector('.inner-slot')).click();
+    expect(instance.emitted()).toBeDefined();
+  });
+
+  test('loading implies disabled', async () => {
+    const instance = mount(Button, {
+      slots: {
+        default: AXIOM
+      },
+      props: { loading: true },
+      ...COMMON_CONFIG
+    });
+    await instance.trigger('click');
+    expect(instance.emitted('click')).toBeUndefined();
   });
 });
--- packages/button/src/index.vue
+++ packages/button/src/index.vue
@@ -1,11 +1,7 @@
 <template>
   <button
-    class="el-button"
-    @click="handleClick"
-    :disabled="buttonDisabled || loading"
-    :autofocus="autofocus"
-    :type="nativeType"
     :class="[
+      'el-button',
       type ? 'el-button--' + type : '',
       buttonSize ? 'el-button--' + buttonSize : '',
       {
@@ -13,78 +9,91 @@
         'is-loading': loading,
         'is-plain': plain,
         'is-round': round,
-        'is-circle': circle,
-      },
+        'is-circle': circle
+      }
     ]"
+    :disabled="buttonDisabled || loading"
+    :autofocus="autofocus"
+    :type="nativeType"
+    @click="handleClick"
   >
-    <i class="el-icon-loading" v-if="loading"></i>
-    <i :class="icon" v-if="icon && !loading"></i>
+    <i v-if="loading" class="el-icon-loading"></i>
+    <i v-if="icon && !loading" :class="icon"></i>
     <span v-if="$slots.default"><slot></slot></span>
   </button>
 </template>
 
-<script lang="ts">
-  import { computed, inject } from 'vue'
+<script lang='ts'>
+import { computed, inject } from 'vue'
+const ELEMENT: {
+  size?: number;
+} = {}
+// TODOS: replace these interface definition with actual ElForm interface
+interface ElForm {
+  disabled: boolean;
+}
+interface ElFormItem {
+  elFormItemSize: number;
+}
 
-  const ELEMENT: { size?: number } = {}
-  interface ElForm {
-    disabled: boolean
-  }
-  interface ElFormItem {
-    elFormItemSize: number
-  }
-  export default {
-    name: 'ElButton',
+export default {
+  name: 'ElButton',
 
-    props: {
-      type: {
-        type: String,
-        default: 'default',
-      },
-      size: String,
-      icon: {
-        type: String,
-        default: '',
-      },
-      nativeType: {
-        type: String,
-        default: 'button',
-      },
-      loading: Boolean,
-      disabled: Boolean,
-      plain: Boolean,
-      autofocus: Boolean,
-      round: Boolean,
-      circle: Boolean,
+  props: {
+    type: {
+      type: String,
+      default: 'default',
     },
-
-    setup(props, ctx) {
-      // inject
-      const elForm = inject<ElForm>('elForm')
-      const elFormItem = inject<ElFormItem>('elFormItem')
-
-      // computed
-      const _elFormItemSize = computed(() => {
-        return (elFormItem || {}).elFormItemSize
-      })
-      const buttonSize = computed(() => {
-        return props.size || _elFormItemSize.value || (ELEMENT || {}).size
-      })
-      const buttonDisabled = computed(() => {
-        return props.disabled || (elForm || {}).disabled
-      })
-
-      // methods
-      const handleClick = (evt) => {
-        ctx.emit('click', evt)
-      }
-
-      return {
-        _elFormItemSize,
-        buttonSize,
-        buttonDisabled,
-        handleClick,
-      }
+    size: {
+      type: String,
+      default: '',
     },
-  }
+    icon: {
+      type: String,
+      default: '',
+    },
+    nativeType: {
+      type: String,
+      default: 'button',
+    },
+    loading: Boolean,
+    disabled: Boolean,
+    plain: Boolean,
+    autofocus: Boolean,
+    round: Boolean,
+    circle: Boolean,
+  },
+
+  emits: ['click'],
+
+  setup(props, ctx) {
+    // inject
+    const elForm = inject<ElForm>('elForm')
+    const elFormItem = inject<ElFormItem>('elFormItem')
+
+    // computed
+    const _elFormItemSize = computed(() => {
+      return (elFormItem || {}).elFormItemSize
+    })
+    const buttonSize = computed(() => {
+      // todo ELEMENT
+      return props.size || _elFormItemSize.value || (ELEMENT || {}).size
+    })
+    const buttonDisabled = computed(() => {
+      return props.disabled || (elForm || {}).disabled
+    })
+
+    //methods
+    const handleClick = (evt) => {
+      ctx.emit('click', evt)
+    }
+
+    return {
+      _elFormItemSize,
+      buttonSize,
+      buttonDisabled,
+      handleClick,
+    }
+  },
+}
 </script>

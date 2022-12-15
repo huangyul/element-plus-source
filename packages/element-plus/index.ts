eeb19215d7bc6b8c2de9272d00bbfe90219ada1d
huangyul <1103221645@qq.com>
2022-12-15 13:37:26 +0800

    feat: add el-divider component

--- packages/element-plus/index.ts
+++ packages/element-plus/index.ts
@@ -1,12 +1,14 @@
 import ElButton from '@element-plus/button';
 import ElBadge from '@element-plus/badge';
 import ElTag from '@element-plus/tag';
+import ElDivider from '@element-plus/divider';
 import { App } from 'vue';
 
 export { ElButton, ElBadge };
 
-export default function(app: App) {
+export default function(app: App): void {
   ElButton(app);
   ElBadge(app);
   ElTag(app);
+  ElDivider(app);
 }
